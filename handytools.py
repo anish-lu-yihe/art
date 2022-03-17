af235d0986f1a343642df1510da1eb526ba0f856
L.Yihe <anish_alpha@hotmail.com>
2022-03-17 14:35:40 +0000

    add savefigure

--- handytools.py
+++ handytools.py
@@ -4,3 +4,12 @@ def interp2coordinates(X, Y, N_interp):
     t = np.linspace(0, 1, N_interp + 2)
     z = [np.interp(t, [0, 1], [x, y]) for x, y in zip(X, Y)]
     return np.transpose(z)[1:-1]
+
+def savefigure_datetime(figure, simname, dirname):
+    from datetime import datetime
+    now = datetime.now()
+    now_str = now.strftime("%Y-%m-%d_%H-%M-%S")
+
+    filename = '{}/{}_{}'.format(dirname, now_str, simname)
+    print("figure saved at", filename)
+    figure.savefig(filename)
