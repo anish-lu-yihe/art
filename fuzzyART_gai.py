4fd0287500d704e3295a4795c057c5e82a43a88e
L.Yihe <anish_alpha@hotmail.com>
2022-03-18 11:00:41 +0000

    add FuzzyARTgai._set_properties

--- fuzzyART_gai.py
+++ fuzzyART_gai.py
@@ -1,4 +1,24 @@
 from fuzzy_art import *
 
 class FuzzyARTgai(FuzzyART):
-    pass
+    def _set_properties(self, alpha=None, gamma=None, rho=None):
+        old_prop = self.alpha, self.beta, self.gamma, self.rho
+        ispropvaried = False
+        if alpha != self.alpha and alpha is not None:
+            self.alpha = alpha
+            self.beta = 1 - alpha
+            ispropvaried = True
+        if gamma != self.gamma and gamma is not None:
+            self.gamma = gamma
+            ispropvaried = True
+        if rho != self.rho and rho is not None:
+            self.rho = rho
+            ispropvaried = True
+
+        new_prop = self.alpha, self.beta, self.gamma, self.rho
+        if ispropvaried:
+            print("""
+                (alpha, beta, gamma, rho) - changed from\n
+                {} to\n
+                {}
+                """.format(old_prop, new_prop))
