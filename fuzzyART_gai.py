f6049651f909666c4d4f5ded00a3a061564ad911
L.Yihe <anish_alpha@hotmail.com>
2022-03-18 11:25:23 +0000

    refine ._set_properties(): use self.__dict__

--- fuzzyART_gai.py
+++ fuzzyART_gai.py
@@ -6,16 +6,11 @@ class FuzzyARTgai(FuzzyART):
         old_prop = self.alpha, self.beta, self.gamma, self.rho
         # update properties
         ispropvaried = False
-        if alpha != self.alpha and alpha is not None:
-            self.alpha = alpha
-            self.beta = 1 - alpha
-            ispropvaried = True
-        if gamma != self.gamma and gamma is not None:
-            self.gamma = gamma
-            ispropvaried = True
-        if rho != self.rho and rho is not None:
-            self.rho = rho
-            ispropvaried = True
+        for propname, new_val in zip(('alpha', 'gamma', 'rho'), (alpha, gamma, rho)):
+            if self.__dict__[propname] != new_val and new_val is not None:
+                self.__dict__[propname] = new_val
+                ispropvaried = True
+        self.beta = 1 - self.alpha
         # log new properties
         new_prop = self.alpha, self.beta, self.gamma, self.rho
         # message about update
@@ -25,5 +20,4 @@ class FuzzyARTgai(FuzzyART):
                 {} to\n
                 {}
                 """.format(old_prop, new_prop))
-
         return ispropvaried
