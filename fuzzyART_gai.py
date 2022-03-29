26a05c18d9ab2e7963d86416c5af934a83eaea58
L.Yihe <anish_alpha@hotmail.com>
2022-03-29 19:07:00 +0100

    add FAg.getcat_vertices()

--- fuzzyART_gai.py
+++ fuzzyART_gai.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from fuzzy_art import *
 
 class FuzzyARTgai(FuzzyART):
@@ -23,11 +25,23 @@ class FuzzyARTgai(FuzzyART):
         return ispropvaried
 
     def getcat_bipolars(self):
-        featnum = self.w.shape[1] // 2
-        return self.w[:, :featnum], 1 - self.w[:, featnum:]
+        self.featnum = self.w.shape[1] // 2
+        return self.w[:, :self.featnum], 1 - self.w[:, self.featnum:]
 
     def getcat_centre(self):
         return np.add(*self.getcat_bipolars()) / 2
 
     def getcat_vertices(self):
-        pass
+        u, v = self.getcat_bipolars()
+        vertnum = 2 ** self.featnum
+        vertices = np.zeros((self.w.shape[0], vertnum, self.featnum))
+        for featidx in range(self.featnum):
+            stride = 2 ** featidx
+            uvidx = np.reshape(np.arange(vertnum), (stride, -1))
+            uidx, vidx = np.split(uvidx, 2, axis=1)
+            vertices[:, uidx.flatten(), featidx] = np.tile(u[:, featidx], (vertnum // 2, 1)).T
+            vertices[:, vidx.flatten(), featidx] = np.tile(v[:, featidx], (vertnum // 2, 1)).T
+
+        return vertices
+
+
