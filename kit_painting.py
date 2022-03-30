7c6d5137d290d2732dd3e8dbdf6076c0cb645fc2
L.Yihe <anish_alpha@hotmail.com>
2022-03-30 11:11:42 +0100

    add photo_pca_truth()

---
+++ kit_painting.py
@@ -0,0 +1,19 @@
+import numpy as np
+from sklearn.decomposition import PCA
+
+def photo_pca_truth(data, label, ax):
+    # PCA
+    pca_dim = 2
+    pca = PCA(n_components=pca_dim)
+    pca_xy = pca.fit_transform(data)
+    pca_msg = "Variance explained by first {} principal components: {}".format(pca_dim, pca.explained_variance_ratio_)
+    print(pca_msg)
+
+    # plot
+    for cat in range(max(label) + 1):
+        ptlb = "cat{}".format(cat)
+        ax.scatter(*np.where(label == cat, pca_xy.T, None), label=ptlb)
+    ax.set_title("true data")
+    ax.legend()
+
+    return pca
