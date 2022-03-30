6b9c1976eb4ddd922c35868ec47e45be04dc251a
L.Yihe <anish_alpha@hotmail.com>
2022-03-30 16:01:13 +0100

    add PaintPCA.tripole()

--- kit_painting.py
+++ kit_painting.py
@@ -10,13 +10,22 @@ class PaintPCA:
         pca_msg = "Variance explained by first {} principal components: {}".format(pca_dim, self.pca.explained_variance_ratio_)
         print(pca_msg)
 
-    def scatter(self, ax, label, data_new=None):
+    def scatter(self, ax, cat_label, data_new=None):
         if data_new is None:
             pca_data = self.pca_data_ref
         else:
             pca_data = self.pca.transform(data_new)
 
-        for cat in range(max(label) + 1):
-            ax.scatter(*np.where(label == cat, pca_data.T, None), label=cat)
+        for cat in range(max(cat_label) + 1):
+            ax.scatter(*np.where(cat_label == cat, pca_data.T, None), label=cat)
 
-
+    def tripole(self, ax, s_pole, n_pole):
+        """
+        input: multiple pairs of s_pole and n_pole
+        """
+        pca_s, pca_n = [self.pca.transform(pole) for pole in (s_pole, n_pole)]
+        pca_c = (pca_s + pca_n) / 2
+        for ps, pc, pn in zip(pca_s, pca_c, pca_n):
+            c = next(ax._get_lines.prop_cycler)['color']
+            ax.plot(*np.transpose([ps, pn]), color=c)
+            ax.scatter(*pc, marker='o', s=200, facecolors='none', linewidths=2, edgecolors=c)
